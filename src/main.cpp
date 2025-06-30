59fbfcc25cfa25e717043149e3b35f081b986628
Lukas <luki.furrer@hotmail.ch>
2025-06-30 14:01:06 +0200

    Add main.cpp as QuickStart

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,44 @@
 #include <raylib.h>
 
-#include <scene_manager.hpp>
-
-int main(void){
-    // Initialization -------------------------------------------------------------------
-    // Screen dimensions
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
-    const int screenHeight = 800;
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(screenWidth, screenHeight, "DungeonSweeper");
-    SetExitKey(KEY_NULL);
-    SetTargetFPS(60);
+    const int screenHeight = 450;
 
-    SceneManager scene;
+    InitWindow(screenWidth, screenHeight, "raylib quick start");
 
-    // RunScene -------------------------------------------------------------------------
-    while (!WindowShouldClose()){
-        scene.Run();
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(DARKBLUE);
+
+            DrawText("Perfect! This is your first raylib window!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
     }
 
-    CloseWindow(); // Close window and OpenGL context
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
 
     return 0;
 }
